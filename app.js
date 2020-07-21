291ad090e97b975d031f1ce08d2a07a84721a6b2
arsalanchishti <shedowmasterno1@gmail.com>
2020-07-21 23:37:56 +0500

    almost ready

---
+++ app.js
@@ -0,0 +1,104 @@
+// ==== 38-42 =====
+
+
+
+
+//==== task no 1 =====
+
+
+// function calc(a,b){
+//     alert(a+b)
+// }
+
+// calc(5,5);
+
+
+
+
+
+// ===== task no 2 ====
+
+// function leye(){
+//     var val1 = prompt('Enter the year');
+//     var sli = val1.slice(3)
+//     if(sli === '0' || sli === '2' || sli === '4' || sli === '6' || sli === '8' ){
+//         alert('This is leap year')
+//     }else{
+//         alert('Sorry this is not leap year')
+//     }
+// }
+
+// leye();
+
+
+
+
+
+
+
+
+
+
+// ===== chepter no 43-48 ========
+
+
+
+
+// ===== task no 1 =====
+
+// function clickbtn(){
+//     alert('this is on click')
+// }
+
+
+
+
+
+
+// ===== task no 2 ====
+
+// answer of this task is on HTML page
+
+
+
+
+
+
+
+// ===== task no 3 =====
+
+// function delet(num){
+//     var table = document.getElementById('mytab');
+//     table.deleteRow(num)
+// }
+
+
+
+
+
+// ===== task no 4 =====
+
+// function swepping(id,src){
+//     var image = document.getElementById(id);
+//     image.src =src
+// }
+
+
+
+
+
+// ===== task no 5 =====
+
+// var val1 = 0;
+// function incre(){
+//     val1++
+//     var inc = document.getElementById('counter');
+//     inc.innerHTML = val1
+// }
+
+
+// function decre(){
+//     val1--
+//     var inc = document.getElementById('counter');
+//     inc.innerHTML = val1
+// }
